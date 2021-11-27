65164b537868427c77730cecc13e04b8d29affe6
Skeletonchampion <greensproblem00@gmail.com>
2021-11-27 15:13:24 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Countdown from 'react-countdown';
 const axios = require('axios');
 axios.defaults.withCredentials = true;
 
-const BACKEND_BASE_URL = "https://sc-backend-reactnotes.netlify.app";
+const BACKEND_BASE_URL = "https://backend-reactnotes.herokuapp.com";
 
 function App() {
   const [notes, setNotes] = React.useState([]);
